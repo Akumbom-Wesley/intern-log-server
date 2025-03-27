f78e4d0b323dac517b844e7ded3e5833dbe282fe
Akumbom Wesley <akumbomwesley7@gmail.com>
2025-03-27 07:44:41 +0100

    Configure swagger

--- config/urls.py
+++ config/urls.py
@@ -14,9 +14,34 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+import config
 from django.contrib import admin
 from django.urls import path
 
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="DuePay API Documentation",
+        default_version='v1',
+        description="API documentation for the DuePay Application",
+        terms_of_service="https://www.nerdtekc.org/policies/intern/terms/",
+        contact=openapi.Contact(email="nerdtekc.org@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny]
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    #swagger
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
