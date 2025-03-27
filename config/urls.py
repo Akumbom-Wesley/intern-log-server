c538844c88d3a044dfb44c86b91b3f28a2b04a4a
Akumbom Wesley <akumbomwesley7@gmail.com>
2025-03-27 14:48:16 +0100

    Configure swagger (#3 Fix static files load)

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
@@ -42,6 +43,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     #swagger
+    path('', RedirectView.as_view(url='/swagger/', permanent=True)),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
